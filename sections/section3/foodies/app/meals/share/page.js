f4c588fe4789a5d9f5f7dcdafe3642fa08873bb7
shiraz <mirzasbaig@nyu.edu>
2025-06-07 17:51:36 -0400

    Section 3 updates - submit meals

--- sections/section3/foodies/app/meals/share/page.js
+++ sections/section3/foodies/app/meals/share/page.js
@@ -1,4 +1,8 @@
+'use client';
+
 import ImagePicker from '@/components/Meals/ImagePicker';
+import MealsFormSubmit from '@/components/Meals/MealsFormSubmit';
+import { shareMeal } from '@/lib/actions';
 
 import classes from './page.module.css';
 
@@ -12,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -40,9 +44,9 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
