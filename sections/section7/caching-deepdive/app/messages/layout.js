5b36ee1b55c8a1cd6963209c66d46bc6f5f36204
shiraz <mirzasbaig@nyu.edu>
2025-07-12 13:14:09 -0400

    section 7 - request memoization learning

---
+++ sections/section7/caching-deepdive/app/messages/layout.js
@@ -0,0 +1,21 @@
+export default async function MessagesLayout({ children }) {
+  const response = await fetch('http://localhost:8080/messages', {
+    // Request memoization test. Removing the header from here and in the page.js file returns
+    // a single cached response for both the layout and page.
+    // This means that the layout and page share the same cache entry.
+    // headers: {
+    //   'X-ID': 'layout',
+    // },
+  });
+  const messages = await response.json();
+  const totalMessages = messages.length;
+
+  return (
+    <>
+      <h1>Important Messages</h1>
+      <p>{totalMessages} messages found</p>
+      <hr />
+      {children}
+    </>
+  );
+}
