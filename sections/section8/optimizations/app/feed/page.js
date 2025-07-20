68e2426971846e4814a111beaedc391a8b8cd48b
shiraz <mirzasbaig@nyu.edu>
2025-07-19 23:20:52 -0400

    add dynamic metadata

--- sections/section8/optimizations/app/feed/page.js
+++ sections/section8/optimizations/app/feed/page.js
@@ -1,6 +1,16 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata(config) {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  const postString = numberOfPosts === 1 ? 'post' : 'posts';
+  return {
+    title: `Browse all our ${numberOfPosts} ${postString}!`,
+    description: 'Browse all our posts',
+  };
+};
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
