cd3ab18d3fdec2148df09f3ef3bc3457b9886140
shiraz <mirzasbaig@nyu.edu>
2025-07-21 22:24:29 -0400

    configure session and session cookie

--- sections/section9/authentication/lib/auth.js
+++ sections/section9/authentication/lib/auth.js
@@ -1,5 +1,6 @@
 import { Lucia } from 'lucia';
 import { BetterSqlite3Adapter } from '@lucia-auth/adapter-sqlite';
+import { cookies } from 'next/headers';
 
 import db from './db';
 
@@ -16,3 +17,13 @@ const lucia = new Lucia(adapter, {
     },
   },
 });
+
+export async function createAuthSession(userId) {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+}
