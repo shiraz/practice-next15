a67fb21215e49b47cdd06fbd8c8d7ce46c3ab308
shiraz <mirzasbaig@nyu.edu>
2025-07-23 18:09:36 -0400

    section 9 complete

--- sections/section9/authentication/actions/auth.js
+++ sections/section9/authentication/actions/auth.js
@@ -2,7 +2,7 @@
 
 import { redirect } from 'next/navigation';
 
-import { createAuthSession } from '@/lib/auth';
+import { createAuthSession, destroySession } from '@/lib/auth';
 import { hashUserPassword, verifyPassword } from '@/lib/hash';
 import { createUser, getUserByEmail } from '@/lib/user';
 
@@ -79,4 +79,9 @@ export async function auth(mode, prevState, formData) {
   }
 
   return signup(prevState, formData);
+}
+
+export async function logout() {
+  await destroySession();
+  redirect('/'); // Redirect to the home page after logout
 }
--- sections/section9/authentication/lib/auth.js
+++ sections/section9/authentication/lib/auth.js
@@ -71,6 +71,24 @@ export async function verifyAuth() {
     console.error('Error validating session:', err);
   }
 
-
   return result;
+}
+
+export async function destroySession() {
+  const { session } = await verifyAuth();
+
+  if (!session) {
+    return {
+      error: 'Unauthorized!',
+    };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
 }
