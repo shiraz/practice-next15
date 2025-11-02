856c3d3e35c2bd30c600bc56c3d9f0dd3e41ec55
shiraz <mirzasbaig@nyu.edu>
2025-11-02 18:15:46 -0500

    section 3 - add image slide show and refactored header

--- sections/redo-section3/foodies/components/MainHeader/index.tsx
+++ sections/redo-section3/foodies/components/MainHeader/index.tsx
@@ -3,24 +3,28 @@ import Link from 'next/link';
 import logoImg from '@/assets/logo.png';
 
 import classes from '../../styles/main-header.module.css';
+import MainHeaderBackground from './main-header-background';
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <img src={logoImg.src} alt="Foodies App Logo" />
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <img src={logoImg.src} alt="Foodies App Logo" />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
